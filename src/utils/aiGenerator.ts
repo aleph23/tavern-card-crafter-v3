0a26a7a950bf7e5623bbf3e210888792823c8dfa
aleph23 <10507562+aleph23@users.noreply.github.com>
2026-02-14 13:36:27 -0800

    modified:   .gitignore 	modified:   electron-builder.json 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	deleted:    public/placeholder.svg 	deleted:    public/robots.txt 	deleted:    public/start-server.bat 	modified:   src/utils/aiGenerator.ts 	deleted:    start-desktop.bat 	modified:   vite.config.ts

--- src/utils/aiGenerator.ts
+++ src/utils/aiGenerator.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-const */
 import { AISettings } from "@/components/AISettings";
 import { buildApiUrl } from "./buildApiUrl";
 import { promptManager } from "./promptManager";
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "./", // important：Electron Need a relative path
   server: {
     host: "::",
-    port: 8080,
+    port: 6090,
   },
   plugins: [
     react(),
